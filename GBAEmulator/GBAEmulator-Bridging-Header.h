0c5f1dfa2290739f7e41085d40193e7a0ae06190
Guilherme Alves <steewprod@gmail.com>
2026-05-10 05:33:17 -0300

    Delete GBAEmulator directory

--- GBAEmulator/GBAEmulator-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-// GBAEmulator-Bridging-Header.h
-// Exposes Objective-C++ bridge to Swift.
-//
-// In Xcode:
-//   Build Settings → Swift Compiler - General → Objective-C Bridging Header
-//   Set to: GBAEmulator/GBAEmulator-Bridging-Header.h
-
-#import "MGBAEmulatorBridge.h"
